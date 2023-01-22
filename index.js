1b7dd064c49e1b6722341c9ff103d7438975c262
vishu-1104 <vishakhayadav528@gmail.com>
2023-01-22 13:10:07 +0530

    changes

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
 document.getElementById("demo").innerHTML = "Hello JavaScript";
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
